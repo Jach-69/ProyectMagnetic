813d88857514d3bf9bb4357719930707a2d101d5
Jhair Anderson Choque <Jhair69tesla@gmail.com>
2024-10-28 02:26:22 -0400

    Mejoras del view Reportes

--- frontend/src/views/accesos/ListAcceso.vue
+++ frontend/src/views/accesos/ListAcceso.vue
@@ -22,13 +22,26 @@
         <option value="true">Sí</option>
         <option value="false">No</option>
       </select>
+    </div>
+
+    <div class="button-container">
+      <div class="descripcion-container">
+        <label for="descripcion">Descripción:</label>
+        <input
+          type="text"
+          v-model="descripcion"
+          id="descripcion"
+          placeholder="Descripción"
+        />
+      </div>
       <button @click="resetFilters" class="reset-button">Borrar Filtros</button>
+      <button @click="generatePDF" class="pdf-button">Generar PDF</button>
     </div>
 
     <table>
       <thead>
         <tr>
-          <th>ID</th>
+          <th>Nro</th>
           <th>Nombre de Persona</th>
           <th>Nombre de Aula</th>
           <th>Fecha y Hora</th>
@@ -36,8 +49,8 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in sortedHistorial" :key="item.id">
-          <td>{{ item.id }}</td>
+        <tr v-for="(item, index) in sortedHistorial" :key="item.id">
+          <td>{{ index + 1 }}</td>
           <td>{{ getPersonaName(item.persona_id) }}</td>
           <td>{{ getAulaName(item.aula_id) }}</td>
           <td>{{ item.fecha_hora }}</td>
@@ -50,6 +63,8 @@
 
 <script>
 import axios from '@/plugins/axios';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
 
 export default {
   data() {
@@ -62,7 +77,8 @@ export default {
         aulaName: '',
         fecha: '',
         esValido: ''
-      }
+      },
+      descripcion: ''
     };
   },
   mounted() {
@@ -76,7 +92,7 @@ export default {
         const personaName = this.getPersonaName(item.persona_id).toLowerCase();
         const aulaName = this.getAulaName(item.aula_id).toLowerCase();
         
-        const fecha = new Date(item.fecha_hora).toISOString().split('T')[0]; // Formato YYYY-MM-DD
+        const fecha = new Date(item.fecha_hora).toISOString().split('T')[0];
 
         const matchesPersonaName = personaName.includes(this.filters.personaName.toLowerCase());
         const matchesAulaName = aulaName.includes(this.filters.aulaName.toLowerCase());
@@ -87,7 +103,7 @@ export default {
       });
     },
     sortedHistorial() {
-      return this.filteredHistorial.slice().sort((a, b) => a.id - b.id);
+      return this.filteredHistorial.slice().sort((a, b) => new Date(b.fecha_hora) - new Date(a.fecha_hora));
     }
   },
   methods: {
@@ -130,6 +146,53 @@ export default {
         fecha: '',
         esValido: ''
       };
+      this.descripcion = '';
+    },
+    async generatePDF() {
+      const pdf = new jsPDF();
+
+      const logoUrl = require('@/../public/logotipo-A.png');
+      const img = new Image();
+      img.src = logoUrl;
+
+      img.onload = async () => {
+        pdf.addImage(img, 'PNG', 14, 10, 50, 20);
+
+        const date = new Date().toLocaleDateString('es-ES');
+        pdf.setFontSize(12);
+        pdf.text(date, 160, 20);
+
+        pdf.setFontSize(22);
+        pdf.text("Historial de Accesos", pdf.internal.pageSize.getWidth() / 2, 40, { align: 'center' });
+
+        // Añadir la descripción con formato
+        pdf.setFontSize(12);
+        pdf.text(`Descripción: ${this.descripcion}`, 14, 50);
+
+        const col = ["Nro", "Nombre de Persona", "Nombre de Aula", "Fecha y Hora", "Es Válido"];
+        const rows = this.sortedHistorial.map((item, index) => [
+          index + 1,
+          this.getPersonaName(item.persona_id),
+          this.getAulaName(item.aula_id),
+          item.fecha_hora,
+          item.es_valido ? 'Sí' : 'No'
+        ]);
+
+        pdf.autoTable(col, rows, {
+          startY: 60,
+          headStyles: { fillColor: [44, 44, 44], textColor: [255, 255, 255] },
+          styles: { fillColor: [255, 255, 255], textColor: [0, 0, 0] }
+        });
+
+        const pageCount = pdf.internal.getNumberOfPages();
+        for (let i = 1; i <= pageCount; i++) {
+          pdf.setPage(i);
+          pdf.setFontSize(10);
+          pdf.text(`Página ${i} de ${pageCount}`, pdf.internal.pageSize.getWidth() - 20, pdf.internal.pageSize.getHeight() - 10, { align: 'right' });
+        }
+
+        pdf.save('historial_de_accesos.pdf');
+      };
     }
   }
 };
@@ -147,7 +210,7 @@ export default {
 .filters {
   margin-bottom: 20px;
   display: flex;
-  align-items: center; /* Alinear verticalmente */
+  align-items: center;
 }
 
 .filters input,
@@ -160,18 +223,47 @@ export default {
   color: white;
 }
 
-.reset-button {
-  padding: 3px 6px; /* Ajuste del tamaño */
-  border: none;
-  border-radius: 4px;
-  background-color: #c0392b; /* Color para el botón */
-  color: white;
-  cursor: pointer;
-  font-size: 0.85rem; /* Tamaño de fuente más pequeño */
+.button-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between; /* Alinear los elementos a los extremos */
+  margin-top: 10px;
 }
 
-.reset-button:hover {
-  background-color: #e74c3c; /* Color al pasar el mouse */
+.descripcion-container {
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+}
+
+.descripcion-container label {
+  margin-right: 5px;
+  color: white; /* Para que el label sea visible */
+}
+
+.reset-button {
+  padding: 3px 6px;
+  border: none;
+  border-radius: 4px;
+  background-color: #c0392b;
+  color: white;
+  cursor: pointer;
+  font-size: 0.85rem;
+}
+
+.pdf-button {
+  padding: 3px 6px;
+  border: none;
+  border-radius: 4px;
+  background-color: #27ae60;
+  color: white;
+  cursor: pointer;
+  font-size: 0.85rem;
+  margin-left: 10px;
+}
+
+.pdf-button:hover {
+  background-color: #2ecc71; 
 }
 
 table {
@@ -191,11 +283,11 @@ th {
 }
 
 tr:nth-child(even) {
-  background-color: #2a2a2a;
+  background-color: #2a2a2a; 
 }
 
 tr:hover {
-  background-color: #3a3a3a;
+  background-color: #3a3a3a; 
 }
 
 tr td {
